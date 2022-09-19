5dd14307bdcd77e52cb7e1f1e3445ef9d2ce5a49
adelaidekangaroo <astkangaroo@gmail.com>
2022-09-19 18:11:59 +0300

    add context test

---
+++ src/test/java/superperk/pipboy/ApplicationTest.java
@@ -0,0 +1,13 @@
+package superperk.pipboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ApplicationTest {
+    @Test
+    void contextLoaded() {
+    }
+}
