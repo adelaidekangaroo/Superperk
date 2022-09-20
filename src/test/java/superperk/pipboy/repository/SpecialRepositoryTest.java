08f09fd2f9f89334c388e36c8e0e8ab65f2cf33c
adelaidekangaroo <astkangaroo@gmail.com>
2022-09-20 11:58:49 +0300

    removed """ feature

--- src/test/java/superperk/pipboy/repository/SpecialRepositoryTest.java
+++ src/test/java/superperk/pipboy/repository/SpecialRepositoryTest.java
@@ -19,8 +19,7 @@ class SpecialRepositoryTest extends AbstractContainerTest {
     private final static Special STRENGTH = Special.builder()
             .id(1L)
             .title("Strength")
-            .description(""" 
-                    Brute physical strength. A high strength score is good for brutal characters.""")
+            .description("Brute physical strength. A high strength score is good for brutal characters.")
             .build();
     private final static Special PERCEPTION = Special.builder()
             .id(2L)
@@ -30,32 +29,27 @@ class SpecialRepositoryTest extends AbstractContainerTest {
     private final static Special ENDURANCE = Special.builder()
             .id(3L)
             .title("Endurance")
-            .description(""" 
-                    Stamina and physical toughness. A character with a high Endurance will survive where others may not.""")
+            .description("Stamina and physical toughness. A character with a high Endurance will survive where others may not.")
             .build();
     private final static Special CHARISMA = Special.builder()
             .id(4L)
             .title("Charisma")
-            .description("""
-                    A combination of appearance and charm. A high Charisma is important for characters that want to influence people with words.""")
+            .description("A combination of appearance and charm. A high Charisma is important for characters that want to influence people with words.")
             .build();
     private final static Special INTELLIGENCE = Special.builder()
             .id(5L)
             .title("Intelligence")
-            .description("""
-                    Knowledge, wisdom and the ability to think quickly. A high Intelligence is important for any character.""")
+            .description("Knowledge, wisdom and the ability to think quickly. A high Intelligence is important for any character.")
             .build();
     private final static Special AGILITY = Special.builder()
             .id(6L)
             .title("Agility")
-            .description("""
-                    Coordination and the ability to move well. A high Agility is important for any active character.""")
+            .description("Coordination and the ability to move well. A high Agility is important for any active character.")
             .build();
     private final static Special LUCK = Special.builder()
             .id(7L)
             .title("Luck")
-            .description("""
-                    Fate. Karma. An extremely high or low Luck will affect the character - somehow. Events and situations will be changed by how lucky (or unlucky) your character is.""")
+            .description("Fate. Karma. An extremely high or low Luck will affect the character - somehow. Events and situations will be changed by how lucky (or unlucky) your character is.")
             .build();
 
     @Rule // reset changes after each test
